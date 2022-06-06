060b2cbddfbdae9c61aa46faa60aaa5a2e04f103
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-06-06 16:48:07 +0300

    add env file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import request, Flask
+from flask import request, Flask, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -7,22 +8,33 @@ def login():
     userName: str = request.form.get("userName")
     password: str = request.form.get("password")
     response: dict= {"token": "61"}
-    return response
+    return jsonify(response)
 
 @app.route("/register", methods=["POST"])
-def login():
+def register():
     userName: str = request.form.get("userName")
     password: str = request.form.get("password")
     response: dict= {"processCode": "1"}
-    return response
+    return jsonify(response)
 
 @app.route("/logout", methods=["POST"])
-def login():
+def logout():
     userName: str = request.form.get("userName")
     response: dict= {"processCode": "1"}
-    return response
+    return jsonify(response)
+
+def readEnvDataAsDict(path: str) ->dict:
+    envVars:dict = {}
+    with open(path, 'r') as file:
+        for line in file:
+            if(line.startswith('#') or not line.strip()):
+                continue
+            key, value = line.strip().split('=',1)
+            envVars[key] = value
+    return envVars
 
 if __name__ == '__main__':
-    host = "127.0.0.1"
-    port = "6061"
+    variables = readEnvDataAsDict('local.env')
+    host = variables.get("BASE_URL")
+    port = variables.get("PORT")
     app.run(host = host, port = port, debug= True)
